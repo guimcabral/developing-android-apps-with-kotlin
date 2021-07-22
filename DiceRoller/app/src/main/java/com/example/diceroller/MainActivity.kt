b974172d3d21b6113cbd0c92cb9fd9e4e96e127b
Guilherme Cabral <guilherme.mcabral@gmail.com>
2021-07-22 06:28:05 -0300

    Substitui o texto por imagens de um dado de 6 lados

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -22,7 +22,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = binding.resultText
-        resultText.text = Random.nextInt(1, 7).toString()
+        val diceImage: ImageView = binding.diceImage
+        diceImage.setImageResource(
+            when (Random.nextInt(1, 7)) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        )
     }
 }
